b86244af8b9d18ad53f65bec3ab92acf83a5c3f4
Jacob Burke <jacobxburke@icloud.com>
2017-11-13 16:45:46 -0600

    Add Navigation Controller to Main.storyboard

---
+++ AttractionDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AttractionDetailViewController.swift
+//  3DTouchViews
+//
+//  Created by Jacob Burke on 11/13/17.
+//  Copyright © 2017 Jacob Burke. All rights reserved.
+//
+
+import UIKit
+
+class AttractionDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
